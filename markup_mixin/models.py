0217e53cf19c9a25b5e3c30da4a5cf6225052d36
mmcardle <mark.mcardle@sohonet.com>
2014-02-05 13:55:48 +0000

    Update models.py
    Remove from django.contrib.markup.templatetags import markup

--- markup_mixin/models.py
+++ markup_mixin/models.py
@@ -2,7 +2,6 @@ import logging
 from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.markup.templatetags import markup
 
 
 MARKUP_HTML = 'h'
